0da5c1f425d47406c5bab6b8fc096b06954d4ece
Gerbrich Ferdinands <gerbrichferdinands@gmail.com>
2019-11-04 09:28:24 +0100

    Adding an example PPG recording

---
+++ R/rec.R
@@ -0,0 +1,17 @@
+#' One example PPG recording
+#'
+#' A dataset containing one example PPG recording of 90 seconds
+#'
+#'
+#' @format A data frame with 3200 rows and 7 variables:
+#' \describe{
+#'   \item{time}{timesteps of the recording, in seconds}
+#'   \item{Red}{red signal}
+#'   \item{Green}{red signal}
+#'   \item{Blue}{red signal}
+#'   \item{X}{movement on x-axis}
+#'   \item{Y}{movement on y-axis}
+#'   \item{Z}{movement on z-axis}
+#' }
+#' @source \url{http://www.heartrateapp.com/}
+"rec"
